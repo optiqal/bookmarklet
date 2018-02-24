34bfe9ed9bc6bf6802fb762074e87b718fd5d754
Darian <darian@optiqal.com>
2018-02-24 01:13:59 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
     script = document.createElement( 'script' );
-   script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'; 
+   script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'; 
     script.onload=releasetheKraken;
     document.body.appendChild(script);
 } 
