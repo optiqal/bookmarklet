a577f0577225c32b5b07e787bf842b100533fb53
Darian <darian@optiqal.com>
2018-02-24 01:23:29 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -10,9 +10,18 @@ else {
  
 function releasetheKraken() {
    console.log("BOOTED UP BABY");
+    let urls = [];
+    
     $('div').each( function() {
       if ( $(this).css('background-image').indexOf('.jpg') > 1 ) {
           console.log("FOUND ONE");
+          urls.push($(this).css('background-image'));
+      }
+      for (i=0; i<urls.length; i++) {
+        var a = $("<a>")
+            .attr("href", urls[i])
+            .attr("download", "img" + 1 + ".jpg")
+            .appendTo("body");
       }
   });
 }
