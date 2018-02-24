1b89a48dd0c69e81f154b6ea9320be27c41aa5e3
Darian <darian@optiqal.com>
2018-02-24 01:04:18 -0500

    Add files via upload

---
+++ bookmarklet.js
@@ -0,0 +1,18 @@
+if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+    script = document.createElement( 'script' );
+   script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'; 
+    script.onload=releasetheKraken;
+    document.body.appendChild(script);
+} 
+else {
+    releasetheKraken();
+}
+ 
+function releasetheKraken() {
+   console.log("BOOTED UP BABY");
+    $('div').each( function() {
+      if ( $(this).css('background-image').indexOf('.jpg') > 1 ) {
+          console.log("FOUND ONE");
+      }
+  });
+}
